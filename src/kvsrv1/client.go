80479f99d0cb0f11098c6b083b9b2357fd86b070
Panshul-Jindal <panshul.jindal7@gmail.com>
2025-11-27 12:24:30 +0530

    lab2 done

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -1,11 +1,12 @@
 package kvsrv
 
 import (
-	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
-	"6.5840/tester1"
-)
+	"time"
 
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
+	tester "6.5840/tester1"
+)
 
 type Clerk struct {
 	clnt   *tester.Clnt
@@ -30,7 +31,22 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
-	return "", 0, rpc.ErrNoKey
+	args := &rpc.GetArgs{Key: key}
+	reply := &rpc.GetReply{}
+
+	for {
+		// Reset reply for every retry
+		*reply = rpc.GetReply{}
+
+		ok := ck.clnt.Call(ck.server, "KVServer.Get", args, reply)
+		if ok {
+			return reply.Value, reply.Version, reply.Err
+		}
+
+		// If RPC failed (network issue), wait briefly and retry
+		time.Sleep(100 * time.Millisecond)
+	}
+
 }
 
 // Put updates key with value only if the version in the
@@ -51,6 +67,32 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // must match the declared types of the RPC handler function's
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
-	// You will have to modify this function.
-	return rpc.ErrNoKey
+	args := &rpc.PutArgs{
+		Key:     key,
+		Value:   value,
+		Version: version,
+	}
+	firstAttempt := true
+	for {
+		reply := &rpc.PutReply{}
+		ok := ck.clnt.Call(ck.server, "KVServer.Put", args, reply)
+
+		if ok {
+			if reply.Err == rpc.ErrVersion {
+				// "If Put receives an ErrVersion on its first RPC, Put should return ErrVersion"
+				if firstAttempt {
+					return rpc.ErrVersion
+				} else {
+					// "If the server returns ErrVersion on a resend RPC... Put must return ErrMaybe"
+					return rpc.ErrMaybe
+				}
+			}
+			return reply.Err
+		}
+
+		// RPC failed; we are now entering retry mode
+		firstAttempt = false
+		time.Sleep(100 * time.Millisecond)
+	}
+
 }
--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,10 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -10,7 +13,9 @@ type Lock struct {
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
 	ck kvtest.IKVClerk
-	// You may add code here
+
+	key  string // The key used in the KV store for this lock
+	myId string // The unique identifier for this client
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +24,82 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
-	// You may add code here
+	lk := &Lock{
+		ck:   ck,
+		key:  l,
+		myId: kvtest.RandValue(8), // Generate unique ID for this lock client
+	}
 	return lk
 }
 
 func (lk *Lock) Acquire() {
-	// Your code here
+	for {
+		// 1. Check current state of the lock
+		val, ver, err := lk.ck.Get(lk.key)
+
+		// 2. Determine if lock is free
+		// It is free if the key doesn't exist yet, or the value is empty string
+		isFree := (err == rpc.ErrNoKey) || (val == "")
+
+		if isFree {
+			// 3. Try to acquire using Optimistic Concurrency Control
+			// If ErrNoKey, version expected is 0. Otherwise use version from Get.
+			targetVer := ver
+			if err == rpc.ErrNoKey {
+				targetVer = 0
+			}
+
+			putErr := lk.ck.Put(lk.key, lk.myId, targetVer)
+
+			if putErr == rpc.OK {
+				// Success: We hold the lock
+				return
+			} else if putErr == rpc.ErrMaybe {
+				// Ambiguous result (dropped reply). Check if we actually won.
+				// We perform a read to see if the value is now our ID.
+				checkVal, _, _ := lk.ck.Get(lk.key)
+				if checkVal == lk.myId {
+					return
+				}
+				// If checkVal != lk.myId, the Put failed or someone else overwrote (unlikely if we follow protocol)
+				// Loop again.
+			}
+			// If ErrVersion, someone else modified it between our Get and Put. Retry loop.
+		} else {
+			// Lock is held by someone else.
+			// Sleep briefly to avoid hammering the server (spin-wait)
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
 }
 
 func (lk *Lock) Release() {
-	// Your code here
+	for {
+		// 1. Get current version to perform atomic delete/clear
+		val, ver, _ := lk.ck.Get(lk.key)
+
+		// Safety check: Only release if we actually hold it
+		// (Though lab instructions imply we can assume we hold it if Release is called)
+		if val == lk.myId {
+			// 2. Set value to empty string to indicate "Free"
+			putErr := lk.ck.Put(lk.key, "", ver)
+
+			if putErr == rpc.OK {
+				return
+			} else if putErr == rpc.ErrMaybe {
+				// Ambiguous result. Check if release happened.
+				checkVal, _, _ := lk.ck.Get(lk.key)
+				if checkVal == "" {
+					return
+				}
+				// If not empty, retry
+			}
+			// If ErrVersion, someone modified it? Retry loop to get new version.
+		} else {
+			// We don't hold the lock, or it's already released.
+			return
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
 }
