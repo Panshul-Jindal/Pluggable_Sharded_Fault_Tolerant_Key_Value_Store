89516e811c1a272eb35f0f1b2a4c778330574f85
AnSaki57 <anakalasashikiran@gmail.com>
2025-11-27 10:26:09 +0530

    Part 2 done

---
+++ src/kvsrv1/common.go
@@ -0,0 +1,35 @@
+// Added for lab 2
+
+
+package kvsrv
+
+// Put or Append
+const (
+	OpPut    = "Put"
+	OpAppend = "Append"
+)
+
+type PutAppendArgs struct {
+	Key   string
+	Value string
+	Op    string // "Put" or "Append"
+
+	// Field names must start with capital letters
+	ClientID  int64
+	RequestID int
+}
+
+type PutAppendReply struct {
+	Value string
+}
+
+type GetArgs struct {
+	Key string
+
+	ClientID  int64
+	RequestID int
+}
+
+type GetReply struct {
+	Value string
+}
--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -18,23 +18,40 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+// Internal struct to hold value and version
+type Item struct {
+	value   string
+	version rpc.Tversion
+}
 
 type KVServer struct {
 	mu sync.Mutex
 
-	// Your definitions here.
+	// Key -> (Value, Version)
+	store map[string]Item
 }
 
 func MakeKVServer() *KVServer {
 	kv := &KVServer{}
-	// Your code here.
+	kv.store = make(map[string]Item)
 	return kv
 }
 
 // Get returns the value and version for args.Key, if args.Key
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	item, exists := kv.store[args.Key]
+	if !exists {
+		reply.Err = rpc.ErrNoKey
+		return
+	}
+
+	reply.Value = item.value
+	reply.Version = item.version
+	reply.Err = rpc.OK
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -42,13 +59,42 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // If the key doesn't exist, Put installs the value if the
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	item, exists := kv.store[args.Key]
+
+	if !exists {
+		// "Put installs the value if the args.Version is 0"
+		if args.Version == 0 {
+			kv.store[args.Key] = Item{
+				value:   args.Value,
+				version: 1, // Version 1 is usually the first valid version after 0
+			}
+			reply.Err = rpc.OK
+		} else {
+			// "returns ErrNoKey otherwise"
+			reply.Err = rpc.ErrNoKey
+		}
+		return
+	}
+
+	// Key exists: Check version match
+	if args.Version == item.version {
+		// Match: update value and increment version
+		item.value = args.Value
+		item.version++
+		kv.store[args.Key] = item
+		reply.Err = rpc.OK
+	} else {
+		// Mismatch
+		reply.Err = rpc.ErrVersion
+	}
 }
 
 // You can ignore Kill() for this lab
 func (kv *KVServer) Kill() {
 }
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []tester.IService {
